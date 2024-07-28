79c7d1c0ddc71a10107d1c58fd760bfc82ede649
Ubuntu <ubuntu@ip-172-31-34-120.us-east-2.compute.internal>
2024-07-28 00:29:04 +0000

    add model checkpointing

--- train.py
+++ train.py
@@ -4,16 +4,22 @@ import numpy as np
 from torch.utils.data import DataLoader
 from dataset import get_data, GPTDataset
 from model import GPT
-import collections
 from tqdm import tqdm
-import ipdb
+import os
 
 # basic hparams
 train_batch_size = 64
 eval_batch_size = 32
 block_size = 32
 epochs = 5
+save_every = 1
+check_output_every = 1
 lr = 5e-4
+
+save_dir = "./checkpoints"
+if not os.path.exists(save_dir):
+  os.makedirs(save_dir)
+
 criterion = nn.CrossEntropyLoss()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -46,10 +52,16 @@ model = GPT(num_blocks=4,
 
 model = model.to(device)
 # print the number of parameters in the model
+print("total model params:")
 print(sum(p.numel() for p in model.parameters())/1e6, 'M parameters')
 
 # create a PyTorch optimizer
 optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+
+# get a model output before any training
+print("untrained output:")
+start_token = torch.zeros((1, 1), dtype=torch.long, device=device)
+print(decode(model.generate(start_token, max_lookback_tokens=block_size, max_new_tokens=100)[0].tolist()))
 
 # training loop
 for epoch in range(epochs):
@@ -57,9 +69,8 @@ for epoch in range(epochs):
   for batch in tqdm(train_dataloader):
     encoded_inputs = batch['encoded_input'].to(device)
     encoded_targets = batch['encoded_target'].to(device)
-
     logits = model(encoded_inputs)
-    
+
     loss = criterion(logits.view(logits.shape[0]*logits.shape[1], logits.shape[2]), encoded_targets.view(-1))
     optimizer.zero_grad()
     loss.backward()
@@ -67,9 +78,23 @@ for epoch in range(epochs):
     optimizer.step()
     losses.append(loss.item())
   mean_epoch_train_loss = np.mean(losses)
+
+  if epoch % check_output_every == 0:
+    print("what're we outputting?")
+    model.eval()
+    start_token = torch.zeros((1, 1), dtype=torch.long, device=device)
+    print(decode(model.generate(start_token, max_lookback_tokens=block_size, max_new_tokens=100)[0].tolist()))
+
+  if epoch % save_every == 0:
+    torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': mean_epoch_train_loss,
+            }, f"{save_dir}/rudimentary_gpt_epoch_{epoch}")
+
   print(f"Epoch {epoch} train loss: {mean_epoch_train_loss}")
 
-ipdb.set_trace()
 
 
 
